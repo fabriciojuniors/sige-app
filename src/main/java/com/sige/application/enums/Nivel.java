e2f885f4e9734b632db12bc6db0ff6e25b0b6e72
Junior Souza <fabriciojuniorsc@gmail.com>
2021-10-07 20:57:47 -0300

    GetAll

--- src/main/java/com/sige/application/enums/Nivel.java
+++ src/main/java/com/sige/application/enums/Nivel.java
@@ -2,7 +2,6 @@ package com.sige.application.enums;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Nivel {
     A("Administrador"),
     C("Cliente"),
