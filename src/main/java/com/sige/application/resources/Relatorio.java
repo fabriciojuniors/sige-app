023d9b90798d0e4ed529899b3792be67e2eb9887
Junior Souza <fabriciojuniorsc@gmail.com>
2021-10-16 15:07:54 -0300

    Relatório de Locais

--- src/main/java/com/sige/application/resources/Relatorio.java
+++ src/main/java/com/sige/application/resources/Relatorio.java
@@ -1,6 +1,9 @@
 package com.sige.application.resources;
 
 import net.sf.jasperreports.engine.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,24 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
+import javax.sql.DataSource;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @RestController()
 @RequestMapping("/rel")
 public class Relatorio {
 
+    @Autowired
+    DataSource dataSource;
+
     public JasperPrint export() throws JRException, FileNotFoundException, SQLException {
-        File file = ResourceUtils.getFile("D:\\Google Drive\\Esucri\\APS\\SIGE\\sige-app\\src\\main\\resources\\TESTE.jrxml");
-        JasperReport s = JasperCompileManager.compileReport(file.getAbsolutePath());
-        Connection c = null;
-        c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sige-app", "postgres", "123");
-        JasperPrint print = JasperFillManager.fillReport(s, null,  new JREmptyDataSource());
+        InputStream jasperStream = this.getClass().getResourceAsStream("/Locais.jrxml");
+        JasperReport s = JasperCompileManager.compileReport(jasperStream);
+        JasperPrint print = JasperFillManager.fillReport(s, null,  dataSource.getConnection());
         return print;
     }
 
@@ -36,8 +41,16 @@ public class Relatorio {
 
     @GetMapping
     public void rel(HttpServletResponse response) throws IOException, JRException, SQLException {
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"teste.pdf\""));
+        /*
+            PARA BAIXAR O PDF DIRETAMENTE
+            response.setContentType("application/x-download");
+            response.setHeader("Content-Disposition", String.format("attachment; filename=\"Locais.pdf\""));
+        */
+
+        //PARA ABRIR O PDF NO NAVEGADOR
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=Locais.pdf");
+
         OutputStream outputStream = response.getOutputStream();
         exportReport(outputStream);
     }
