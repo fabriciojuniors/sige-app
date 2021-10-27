c3cf393428b31b557e20c76e7369ca9be077cb6f
Junior Souza <fabriciojuniorsc@gmail.com>
2021-10-27 20:00:01 -0300

    Criação de parceiro

--- src/main/java/com/sige/application/controller/ParceiroController.java
+++ src/main/java/com/sige/application/controller/ParceiroController.java
@@ -5,6 +5,7 @@ import com.sige.application.exception.CampoException;
 import com.sige.application.model.Endereco;
 import com.sige.application.model.Local;
 import com.sige.application.model.Parceiro;
+import com.sige.application.model.Usuario;
 import com.sige.application.repository.EnderecoRepository;
 import com.sige.application.repository.ParceiroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ParceiroController {
@@ -23,6 +26,16 @@ public class ParceiroController {
     EnderecoRepository enderecoRepository;
 
     public Parceiro save(Parceiro parceiro){
+
+        List<Parceiro> validaCPF = repository.findByCpf(parceiro.getCpf());
+
+        if(!validaCPF.isEmpty()){
+            for(Parceiro parceiro1 : validaCPF){
+                if(!parceiro.equals(parceiro1)){
+                    throw new CampoException("parceiro.cpf", "CPF informado já cadastrado.", "", ExceptionOperacao.C);
+                }
+            }
+        }
 
         Endereco endereco = enderecoRepository.save(parceiro.getEndereco());
         parceiro.setEndereco(endereco);
--- src/main/java/com/sige/application/repository/ParceiroRepository.java
+++ src/main/java/com/sige/application/repository/ParceiroRepository.java
@@ -3,5 +3,9 @@ package com.sige.application.repository;
 import com.sige.application.model.Parceiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParceiroRepository extends JpaRepository<Parceiro, Long> {
+
+    List<Parceiro> findByCpf(String cpf);
 }
