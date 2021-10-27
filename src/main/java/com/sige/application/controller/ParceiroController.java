df8fd0dcca2927c658f70bf46de248ac14941524
Junior Souza <fabriciojuniorsc@gmail.com>
2021-10-27 19:00:17 -0300

    Criação de parceiro

--- src/main/java/com/sige/application/controller/ParceiroController.java
+++ src/main/java/com/sige/application/controller/ParceiroController.java
@@ -1,7 +1,14 @@
 package com.sige.application.controller;
 
+import com.sige.application.enums.ExceptionOperacao;
+import com.sige.application.exception.CampoException;
+import com.sige.application.model.Local;
+import com.sige.application.model.Parceiro;
 import com.sige.application.repository.ParceiroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +16,23 @@ public class ParceiroController {
 
     @Autowired
     ParceiroRepository repository;
+
+    public Parceiro save(Parceiro parceiro){
+        return repository.save(parceiro);
+    }
+
+    public Page<Parceiro> getAll(int pagina){
+        Pageable pageable = PageRequest.of(pagina-1, 5);
+        return repository.findAll(pageable);
+    }
+
+    public void delete(int id){
+        Parceiro parceiro = repository.findById((long) id).get();
+        if(parceiro == null){
+            throw new CampoException("parceiro.id", "Não foi possível excluir o parceiro.", "Não encontrado parceiro com ID " + id + " para exclusão.", ExceptionOperacao.D);
+        }else{
+            repository.delete(parceiro);
+        }
+    }
 
 }
--- src/main/java/com/sige/application/resources/ParceiroResource.java
+++ src/main/java/com/sige/application/resources/ParceiroResource.java
@@ -1,9 +1,12 @@
 package com.sige.application.resources;
 
 import com.sige.application.controller.ParceiroController;
+import com.sige.application.model.Parceiro;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(path = "/parceiro")
@@ -11,5 +14,20 @@ public class ParceiroResource {
 
     @Autowired
     ParceiroController controller;
+
+    @PostMapping
+    public Parceiro save(@RequestBody Parceiro parceiro){
+        return controller.save(parceiro);
+    }
+
+    @GetMapping
+    public Page<Parceiro> getAll(@PathParam("pagina") int pagina){
+        return controller.getAll(pagina);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable int id){
+        controller.delete(id);
+    }
 
 }
