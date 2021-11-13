cd276d99d2386ea4609a7692431292551c8bd20a
Junior Souza <fabriciojuniorsc@gmail.com>
2021-11-13 20:33:27 -0300

    Finalizar carrinho PIX.

--- src/main/java/com/sige/application/resources/CarrinhoResource.java
+++ src/main/java/com/sige/application/resources/CarrinhoResource.java
@@ -122,13 +122,17 @@ public class CarrinhoResource {
         }
 
         int invalidos = 0;
+        int invalidosUsuario = 0;
         for(ItemCarrinho itemCarrinho : carrinho.getItemCarrinhos()){
             if(itemCarrinho.getIngresso().getCpf().equals("") || itemCarrinho.getIngresso().getNome().equals("")){
                 invalidos++;
             }
+            if(itemCarrinho.getIngresso().getCpf().equals(carrinho.getUsuario().getCpf()) || itemCarrinho.getIngresso().getNome().equals(carrinho.getUsuario().getNome())){
+                invalidosUsuario++;
+            }
         }
 
-        if(invalidos > 0){
+        if(invalidos > 0 || invalidosUsuario > 1){
             throw new CampoException("ingresso", "Existem ingressos sem nome ou CPF", "Existem ingressos sem nome ou CPF", ExceptionOperacao.C);
         }
 
