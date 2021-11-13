815983e747dbd77895d06fd51d2b2d62b8c41060
Junior Souza <fabriciojuniorsc@gmail.com>
2021-11-13 14:24:01 -0300

    Ajustado validação relacionada a quantidade permitida

--- src/main/java/com/sige/application/resources/CarrinhoResource.java
+++ src/main/java/com/sige/application/resources/CarrinhoResource.java
@@ -88,7 +88,7 @@ public class CarrinhoResource {
                 Logger.getLogger("EVENTO").info("Permitida venda: " + permitidaVenda);
 
                 if(vendido >= permitidaLocal  || previsao > permitidaLocal){
-                    if(permitidaLocal < vendido){
+                    if(permitidaLocal > vendido){
                         throw new CampoException("local.capacidade", "Para este evento os ingressos disponíveis são " + permitidaVenda, "Para este evento os ingressos disponíveis são " + permitidaVenda, ExceptionOperacao.C);
                     }else{
                         throw new CampoException("local.capacidade", "Para este evento já foram vendidos todos os ingressos.", "Para este evento já foram vendidos todos os ingressos.", ExceptionOperacao.C);
